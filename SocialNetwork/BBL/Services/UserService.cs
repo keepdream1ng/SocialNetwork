8f1c69d195bcbbd29c5e5e20e52fbab5aa4df2f9
Anatolii Ivanov <gotnickname@gmail.com>
2023-07-01 15:41:27 +0600

    Add project files.

---
+++ SocialNetwork/BBL/Services/UserService.cs
@@ -0,0 +1,69 @@
+﻿using SocialNetwork.BBL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using SocialNetwork.DAL.Repositories;
+using SocialNetwork.DAL.Entities;
+
+namespace SocialNetwork.BBL.Services
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void Register(UserRegistrationData userRegistrationData)
+        {
+            if (String.IsNullOrEmpty(userRegistrationData.FirstName))
+            {
+                throw new ArgumentNullException(nameof(userRegistrationData.FirstName));
+            }
+            if (String.IsNullOrEmpty(userRegistrationData.LastName))
+            {
+                throw new ArgumentNullException(nameof(userRegistrationData.LastName));
+            }
+            if (String.IsNullOrEmpty(userRegistrationData.Password))
+            {
+                throw new ArgumentNullException(nameof(userRegistrationData.Password));
+            }
+            if (String.IsNullOrEmpty(userRegistrationData.Email))
+            {
+                throw new ArgumentNullException(nameof(userRegistrationData.Email));
+            }
+
+            if (userRegistrationData.Password.Length < 8)
+            {
+                throw new ArgumentException("Wrong password lenght, less than 8 characters", nameof(userRegistrationData.Password));
+            }
+            if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email))
+            {
+                throw new ArgumentException("Email is not valid", nameof(userRegistrationData.Email));
+            }
+
+            if (_userRepository.FindByEmail(userRegistrationData.Email) is not null)
+            {
+                throw new ArgumentException("Email is already registered", nameof(userRegistrationData.Email));
+            }
+
+            var userEntity = new UserEntity()
+            {
+                firstname = userRegistrationData.FirstName,
+                lastname = userRegistrationData.LastName,
+                email = userRegistrationData.Email,
+                password = userRegistrationData.Password
+            };
+
+            if (_userRepository.Create(userEntity) == 0)
+            {
+                throw new Exception("Registration failed");
+            }
+        }
+    }
+}
---
+++ SocialNetwork/Program.cs
@@ -0,0 +1,53 @@
+﻿using SocialNetwork.BBL.Models;
+using SocialNetwork.BBL.Services;
+using SocialNetwork.DAL.Repositories;
+
+namespace SocialNetwork
+{
+    internal class Program
+    {
+
+        public static UserService userService = new UserService(new UserRepository());
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                UserRegistrationPrompt();
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void UserRegistrationPrompt()
+        {
+            Console.WriteLine("Welcome to our console social network");
+            var userData = new UserRegistrationData();
+            Console.Write("Incert first name for registration: ");
+            userData.FirstName = Console.ReadLine();
+            Console.Write("Now incert last name: ");
+            userData.LastName = Console.ReadLine();
+            Console.Write("Now incert password (8+ characters): ");
+            userData.Password = Console.ReadLine();
+            Console.Write("Now incert email: ");
+            userData.Email = Console.ReadLine();
+
+            try
+            {
+                userService.Register(userData);
+                Console.WriteLine("Registration went successfully!");
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine($"{ex.ParamName} is empty");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Enter correct value of {ex.ParamName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
