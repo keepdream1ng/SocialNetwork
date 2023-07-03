bfb98e335388b18b341074f6b7c98f7ae28c001a
Anatolii Ivanov <gotnickname@gmail.com>
2023-07-03 16:08:03 +0600

    added UI methods

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -20,7 +20,10 @@ namespace SocialNetwork
                 {
                     case "1":
                         {
-                            UserAuthentification();
+                            if (UserAuthentification(out User user))
+                            {
+                                UserControlOptions(user);
+                            }
                             break;
                         }
                     case "2":
@@ -67,7 +70,7 @@ namespace SocialNetwork
             }
         }
 
-        public static void UserAuthentification()
+        public static bool UserAuthentification(out User user)
         {
             var authenticationData = new UserAuthenticationData();
 
@@ -79,13 +82,13 @@ namespace SocialNetwork
 
             try
             {
-                User user = userService.Authenticate(authenticationData);
+                user = userService.Authenticate(authenticationData);
 
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"You succesfully signed in, {user.FirstName}");
                 Console.ForegroundColor = ConsoleColor.White;
 
-                UserControlOptions();
+                return true;
             }
             catch (WrongPasswordException)
             {
@@ -99,11 +102,87 @@ namespace SocialNetwork
                 Console.WriteLine("This email is not registered!");
                 Console.ForegroundColor = ConsoleColor.White;
             }
+            user = null;
+            return false;
         }
 
-        public static void UserControlOptions()
+        public static void UserControlOptions(User user)
         {
+            while (true)
+            {
 
+                Console.WriteLine("See your profile info (press 1)");
+                Console.WriteLine("Edit profile (press 2)");
+                Console.WriteLine("Add a friend (press 3)");
+                Console.WriteLine("Write a message (press 4)");
+                Console.WriteLine("Exit profile (press 5)");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        {
+                            ShowProfileInfo(user);
+                            break;
+                        }
+                    case "2":
+                        {
+                            ChangeProfileDetails(user);
+                            break;
+                        }
+                    case "3":
+                        {
+                            throw new NotImplementedException();
+                            break;
+                        }
+                    case "4":
+                        {
+                            throw new NotImplementedException();
+                            break;
+                        }
+                    case "5":
+                        {
+                            return;
+                        }
+                }
+            }
+        }
+
+        public static void ShowProfileInfo(User user)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("User profile info");
+            Console.WriteLine("User Id: {0}", user.Id);
+            Console.WriteLine("First name: {0}", user.FirstName);
+            Console.WriteLine("Last name: {0}", user.LastName);
+            Console.WriteLine("Email: {0}", user.Email);
+            Console.WriteLine("Photo link: {0}", user.Photo);
+            Console.WriteLine("Favorite movie: {0}", user.FavoriteMovie);
+            Console.WriteLine("Favorite book: {0}", user.FavoriteBook);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void ChangeProfileDetails(User user)
+        {
+            Console.Write("My name is: ");
+            user.FirstName = Console.ReadLine();
+
+            Console.Write("My last name is: ");
+            user.LastName = Console.ReadLine();
+
+            Console.Write("Link to my photo: ");
+            user.Photo = Console.ReadLine();
+
+            Console.Write("My favorite movie is: ");
+            user.FavoriteMovie = Console.ReadLine();
+
+            Console.Write("My favorite book is: ");
+            user.FavoriteBook = Console.ReadLine();
+
+            userService.Update(user);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Profile details updated succesfully!");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
