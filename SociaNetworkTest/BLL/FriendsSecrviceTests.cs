aa32327fe479a32d46d6e5cfe582a761ce631279
Anatolii Ivanov <gotnickname@gmail.com>
2023-07-07 22:00:07 +0600

    added test for FriendsService business logic

---
+++ SociaNetworkTest/BLL/FriendsSecrviceTests.cs
@@ -0,0 +1,28 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SociaNetworkTest.BLL
+{
+    public class FriendsSecrviceTests
+    {
+        [Fact]
+        public void AddToFriends_ShouldThrowUserNotFoundException()
+        {
+            FriendGenerationData friendData = new FriendGenerationData() { UserId = 1, FriendEmail = "test@mail.com" };
+
+            var moqUserRepository = new Mock<IUserRepository>();
+            moqUserRepository.Setup(r => r.FindByEmail(friendData.FriendEmail)).Returns<User>(null);
+
+            FriendsService friendsService = new FriendsService(null, moqUserRepository.Object, null);
+
+            Assert.Throws<UserNotFoundException>(() => friendsService.AddToFriends(friendData));
+        }
+    }
+}
